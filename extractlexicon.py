14da01d24cdc696532d5c11ca65234217e9f9046
Johan Winge <johan.winge@gmail.com>
2015-11-29 11:43:30 +0100

    Train RFtagger without calling Morpheus.

--- extractlexicon.py
+++ extractlexicon.py
@@ -4,27 +4,16 @@
 import postags
 import codecs
 
-morpheus = codecs.open("vocabulary-crunched.txt","r","utf8")
+macronsfile = codecs.open("macrons.txt","r","utf8")
 lexicon = codecs.open("rftagger-lexicon.txt","w","utf8")
 
 tagtoaccents = {}
 
-for wordform in morpheus:
-    wordform = wordform.strip()
-    NLs = morpheus.next().strip()
-    parses = []
-    for NL in NLs.split("<NL>"):
-        NL = NL.replace("</NL>","")
-        NLparts = NL.split()
-        if len(NLparts) > 0:
-            parses += postags.Morpheus2Parses(wordform,NL)
-    for parse in parses:
-        lemma = parse[postags.LEMMA].replace("#","").replace("1","").replace(" ","+")
-        accented = parse[postags.ACCENTEDFORM]
-        tag = postags.Parse2LDT(parse)
-        tagtoaccents[tag] = tagtoaccents.get(tag,[]) + [postags.unicodeaccents(accented)]
-        tag = '.'.join(list(tag))
-        lexicon.write(wordform + '\t' + tag + '\t' + lemma + '\n')
+for line in macronsfile:
+    [wordform, tag, lemma, accented] = line.split()
+    tagtoaccents[tag] = tagtoaccents.get(tag,[]) + [postags.unicodeaccents(accented)]
+    tag = '.'.join(list(tag))
+    lexicon.write(wordform + '\t' + tag + '\t' + lemma + '\n')
 
 def escapedaccents(txt):
     for replacement, source in [("a_",u"ā"),("e_",u"ē"),("i_",u"ī"),("o_",u"ō"),("u_",u"ū"),("y_",u"ȳ"),
--- train-rftagger.sh
+++ train-rftagger.sh
@@ -1,5 +1,4 @@
-MORPHLIB=morpheus/stemlib morpheus/bin/cruncher -LS < vocabulary.txt > vocabulary-crunched.txt 
-python morpheus2lexicon.py
+python extractlexicon.py
 python corpus2train.py
 rft-train -l rftagger-lexicon.txt -c 7 -p 6.5 ldt-corpus.txt wordclass.txt rftagger-ldt.model
 
